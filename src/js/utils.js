aee501a4318eb8617b5801979b65fe257725e44a
Cong Vu <congliv.nitc.neu@gmail.com>
2023-06-14 23:33:00 +0700

    update utils

--- src/js/utils.js
+++ src/js/utils.js
@@ -11,7 +11,7 @@ export class Utils{
             a.getGlobalPosition().x - a.width / 2 < b.getGlobalPosition().x + b.width / 2 &&
             a.getGlobalPosition().x  + a.width / 2 > b.getGlobalPosition().x - b.width / 2 &&
             a.getGlobalPosition().y - a.height / 2 < b.getGlobalPosition().y + b.height / 2 &&
-            a.getGlobalPosition().y + a.width / 2 > b.getGlobalPosition().y - b.height / 2
+            a.getGlobalPosition().y + a.height / 2 > b.getGlobalPosition().y - b.height / 2
         );
     }
 
